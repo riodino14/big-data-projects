93b39844bf0405fb5252634b00efdeb830d3ce99
Riodino Raihan <riodinoraihan@gmail.com>
2026-09-20 15:39:50 +0900

    completing running to test the program

--- individu/mapreduce_word_frequency/mapper.py
+++ individu/mapreduce_word_frequency/mapper.py
@@ -4,6 +4,6 @@ for line in sys.stdin:
     words = line.strip().lower().split()
 
     for word in words:
-        word = word.strip(".,!?;:"'()[]{}")
+        word = word.strip(".,!?;:\"'()[]{}")
         if word:
             print(f"{word}\t1")
